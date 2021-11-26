2894a9a5d29a7e7f00495e9322f199c7ecb63f5d
Seungjae Park <astro.psj@gmail.com>
2021-11-26 14:54:30 +0900

    Update workflow and fix error

--- qp-postgres/tests/pool.rs
+++ qp-postgres/tests/pool.rs
@@ -2,10 +2,15 @@ use tokio_postgres::NoTls;
 
 #[tokio::test]
 async fn main() {
-    let config = "host=localhost user=postgres password=postgres".parse().unwrap();
+    let config = "host=localhost user=postgres password=postgres"
+        .parse()
+        .unwrap();
     let pool = qp_postgres::connect(config, NoTls, 4);
     let conn = pool.acquire().await.unwrap();
-    let rows = conn.query("SELECT $1::TEXT", &[&"hello world"]).await.unwrap();
+    let rows = conn
+        .query("SELECT $1::TEXT", &[&"hello world"])
+        .await
+        .unwrap();
     let value: &str = rows[0].get(0);
     assert_eq!(value, "hello world");
 }
